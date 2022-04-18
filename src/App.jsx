f5ecbdd3c73533f2c0dc7a6a57be4c1b8fb613c9
Sergej <sergej.kubat18@gmail.com>
2022-04-18 05:36:28 +0200

    Basic changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,26 @@
+import { useAddress, useMetamask } from "@thirdweb-dev/react";
+
 const App = () => {
+  const address = useAddress();
+  const connectWithMetamask = useMetamask();
+  console.log("👋 Address:", address);
+
+  if (!address) {
+    return (
+      <div className="landing">
+        <h1>Welcome to PizzaDAO</h1>
+        <button onClick={connectWithMetamask} className="btn-hero">
+          Connect your wallet
+        </button>
+      </div>
+    );
+  }
+
+  // This is the case where we have the user's address
+  // which means they've connected their wallet to our site!
   return (
     <div className="landing">
-      <h1>Welcome to My DAO</h1>
+      <h1>👀 wallet connected, now what!</h1>
     </div>
   );
 };
